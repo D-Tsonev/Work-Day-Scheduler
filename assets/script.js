e400306fe928c763519867d1118e7318a1b44c07
Dimi Tsonev <dconev@gmail.com>
2023-12-10 20:02:11 +0000

    current time function showing presant,past or future

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,60 @@
-const currentDayElement = $('#currentDay');
+const currentDayElement = $('#currentDay')
+const descriptionEl = $('description')
+const saveButtonEl = $('saveBtn')
+
 
 dayjs.extend(window.dayjs_plugin_advancedFormat)
-const gradDate = dayjs().format('Do MMMM YYYY');;
-currentDayElement.text(gradDate);
+
+
+const gradDate = dayjs().format('Do MMMM YYYY')
+
+// Showing day, month and year in format  1st December 2023
+currentDayElement.text(gradDate)
+
+function timeColoring (){
+
+}
+
+currentTime = dayjs().format('hA')
+
+
+function handleCurrentTime(SelectedTime) {
+
+  console.log(`Current time: ${currentTime}`);
+  console.log(typeof currentTime)
+
+  if (SelectedTime > currentTime ){
+    console.log ('future')}
+  else if (SelectedTime < currentTime ){
+    console.log ('past')}
+  else {
+    console.log('presant ')
+  }
+  }
+
+
+
+handleCurrentTime('7PM');
+
+// handleCurrentTime (8)
+
+function handleSaveButton (event) {
+  event.preventDefault()
+  // reading the value in description field
+  description = descriptionEl.val.trim()
+
+  //  add it ot localstorage 
+  description.reset()
+}
+
+// saveButtonEl.on('click', handleSaveButton)
+
+function handleDelete (event){
+  console.log(event.target)
+  let deleteDescription = $(event.target)
+  deleteDescription.empty()
+
+  // remove it from localstore
+
+
+}

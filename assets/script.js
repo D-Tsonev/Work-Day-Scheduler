a28a22df88dd493c51eb2f47cadc650e4650e191
Dimi Tsonev <dconev@gmail.com>
2023-12-10 18:48:56 +0000

    creaded divs for 9-5

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 const currentDayElement = $('#currentDay');
 
-
-
 dayjs.extend(window.dayjs_plugin_advancedFormat)
 const gradDate = dayjs().format('Do MMMM YYYY');;
 currentDayElement.text(gradDate);

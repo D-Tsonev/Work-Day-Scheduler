0d393f96a0dd6d43e791bd876e9429df31094ce6
Dimi Tsonev <dconev@gmail.com>
2023-12-11 17:03:56 +0000

    showing the text in decription filed in console

--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,73 @@
+
 const currentDayEl = $('#currentDay')
-const descriptionEl = $('.description')
-const saveButtonEl = $('saveBtn')
-const hourSelector = $('.hour').text()
-const timeContainerEls = $('.row.time-block');
 const mainContainer = $('.container')
 
+$(document).ready(function () {
 let currentTime = dayjs()
 
-let currentTime24 = currentTime.format('HH');
-
-
-
-currentTime = dayjs().format('hA')
-
-
+let currentTime24 = parseInt(currentTime.format('HH'))
+console.log(currentTime24)
 
 dayjs.extend(window.dayjs_plugin_advancedFormat)
-
-
 const showDate = dayjs().format('Do MMMM YYYY')
 
-// console.log(currentTime12);
-
-
-// console.log(currentTime24Hour)
-
-for (let i = 9; i < 18; i++) {
-  const hour = dayjs().hour(i).format("hA");;
-  console.log(i);
-
-  let hourDiv = $("<div>").addClass('row time-block');
-  let hourText = $('<div>').addClass('col-md-1 hour').text(hour);
-  let description = $('<textarea>').addClass('col-md-10 description');
-  let saveButton = $('<button>').addClass('btn saveBtn col-md-1');
-  let faIcon = $('<span>').addClass('far fa-save fa-lg');
-  saveButton.append(faIcon);
-
-  if (i > currentTime24) {
-    hourDiv.addClass('future');
-    console.log('future');
-  } else if (i < currentTime24) {
-    hourDiv.addClass('past');
-    console.log('past');
-  } else {
-    console.log('present');
-    hourDiv.addClass('present');
-  }
-
-  mainContainer.append(hourDiv);
-  hourDiv.append(hourText);
-  hourDiv.append(description);
-  hourDiv.append(saveButton);
-}
-
-
-
-
-
-
-
-
-// Showing day, month and year in format  1st December 2023
+// Showing day, month and year in format  1st January 2023
 currentDayEl.text(showDate)
 
 
+// Loop though hours from 9 to 17 
+for (let i = 9; i < 18; i++) {
 
+//  Current time in 12hour format 
+  const hour = dayjs().hour(i).format("hA")
 
+  // Creating a dynamic HTML elements
 
+  let hourDiv = $("<div>").addClass('row time-block')
+  let hourText = $('<div>').addClass('col-md-1 hour').text(hour)
+  let description = $('<textarea>').addClass('col-md-10 description')
+  let saveButton = $('<button>').addClass('btn saveBtn col-md-1')
+  let faIcon = $('<span>').addClass('far fa-save fa-lg')
+  saveButton.append(faIcon)
 
+  if (i > currentTime24) {
+    hourDiv.addClass('future')
+  } else if (i < currentTime24) {
+    hourDiv.addClass('past')
+  } else {
+    hourDiv.addClass('present')
+  }
 
-function handleSaveButton (event) {
-  event.preventDefault()
-  // reading the value in description field
-  description = descriptionEl.val.trim()
-
-  //  add it ot localstorage 
-  description.reset()
+  mainContainer.append(hourDiv)
+  hourDiv.append(hourText)
+  hourDiv.append(description)
+  hourDiv.append(saveButton)
 }
 
-// saveButtonEl.on('click', handleSaveButton)
 
-function handleDelete (event){
-  console.log(event.target)
-  let deleteDescription = $(event.target)
-  deleteDescription.empty()
+function handleSaveButton(event) {
+  event.preventDefault();
+  
+  let task = $(this).siblings(".description").val().trim();
+  let hour = $(this).siblings(".hour").text();
 
-  // remove it from localstore
+  console.log(task, hour)
 
+
+
+  // Add it to local storage
 
 }
+
+$('.saveBtn').on('click', handleSaveButton);
+
+})
+
+
+// function handleDelete (event){
+//   console.log(event)
+//   let deleteDescription = $(event.target)
+//   deleteDescription.empty()
+
+//   // remove it from localstore
+// }
